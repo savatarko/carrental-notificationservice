679e160d52b263ca36af4647e26077e15f8f8d98
savatarko <ivkovic3@gmail.com>
2023-01-10 13:20:00 +0100

    Initial commit

---
+++ src/main/java/org/komponente/notificationservice/listener/EmailListener.java
@@ -0,0 +1,27 @@
+package org.komponente.notificationservice.listener;
+
+import org.komponente.notificationservice.listener.helper.MessageHelper;
+import org.komponente.notificationservice.service.EmailService;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+
+@Component
+public class EmailListener {
+
+    private MessageHelper messageHelper;
+    private EmailService emailService;
+
+    public EmailListener(MessageHelper messageHelper, EmailService emailService) {
+        this.messageHelper = messageHelper;
+        this.emailService = emailService;
+    }
+
+    @JmsListener(destination = "${destination.sendEmails}", concurrency = "5-10")
+    public void addOrder(Message message) throws JMSException {
+
+    }
+
+}
